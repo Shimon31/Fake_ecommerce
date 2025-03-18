0386ddf53ffef892145121e90f25c27b32184ab2
Shimon <shimonbhuiya02@gmail.com>
2025-03-18 14:10:47 +0600

    create repo

---
+++ app/src/main/java/com/example/fake_store/FakeEComApp.kt
@@ -0,0 +1,8 @@
+package com.example.fake_store
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FakeEComApp :Application(){
+}
---
+++ app/src/main/java/com/example/fake_store/repos/GetProductsRepo.kt
@@ -0,0 +1,14 @@
+package com.example.fake_store.repos
+
+import com.example.fake_store.api.ProductService
+import com.example.fake_store.data.product.ResponseProductItem
+import retrofit2.Response
+import javax.inject.Inject
+
+class GetProductsRepo @Inject constructor(private val service: ProductService) {
+
+    suspend fun getAllProducts() : Response<List<ResponseProductItem>>{
+        return service.getAllProducts()
+    }
+
+}
